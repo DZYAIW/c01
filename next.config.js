289e4dbe8f40d9f7d80a78e641d72f8d3360605b
DZYAIW <31542199@qq.com>
2025-01-13 11:05:36 +0800

    Local changes to Sidebar.tsx

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: []
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
+};
+
+module.exports = nextConfig; 
